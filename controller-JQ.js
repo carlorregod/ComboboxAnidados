1cf29e39cd12e64132f166153b8b6c9ab0a3b787
Carlos <carlorregod@gmail.com>
2019-04-19 08:47:33 -0400

    Combobox Encapsulado v1.0

--- controller-JQ.js
+++ controller-JQ.js
@@ -1,52 +1,80 @@
-ajaxCallback = function(params, method, url, callback, asynchr=true )
-{
-        $.ajax({
-                asynchr: asynchr,
-                method: method,
-                url: url,
-                data: params
-                //dataType: 'mycustomtype'
-        })
-        .done(function(respuesta) {
-                callback(respuesta);
-        })
-        .fail(function() {
-                console.log('Ha fallado...');
-        });
-        return false;
-};
-
-callback_cmbPais = function(data) {
-    $('#comboboxpais').html(data);
-    $('#comboboxciudad').html('<option value="0">Seleccionar Opción</option>');
-};
-
-callback_cmbCiudad = function(data) {
-    $('#comboboxciudad').html(data);
-};
-
-inicial = function() {
-    params = {
-      'comando':'carga_comboboxPais'  
+var Capsula = function() {
+    //Privatización de métodos
+    _ajaxCallback = function(params, method, url, callback, asynchr=true )
+    {
+            $.ajax({
+                    asynchr: asynchr,
+                    method: method,
+                    url: url,
+                    data: params
+                    //dataType: 'mycustomtype'
+            })
+            .done(function(respuesta) {
+                    callback(respuesta);
+            })
+            .fail(function() {
+                    console.log('Ha fallado...');
+            });
+            return false;
     };
-    method = 'POST';
-    url = 'modelado.php';
-    ajaxCallback(params, method, url, callback_cmbPais);
+    
+    _callback_cmbPais = function(data) {
+        $('#comboboxpais').html(data);
+        $('#comboboxciudad').html('<option value="0">Seleccionar Opción</option>');
+    };
+    
+    _callback_cmbCiudad = function(data) {
+        $('#comboboxciudad').html(data);
+    };
+       
+    //Publicos
+    this.ajaxCallback = function(params, method, url, callback, asynchr ) {
+        return _ajaxCallback(params, method, url, callback, asynchr);
+    };
+    
+    this.callback_cmbPais = function(data) {
+        return _callback_cmbPais(data);
+    };
+    
+    this.callback_cmbCiudad = function(data) {
+        return _callback_cmbCiudad(data);
+    };
+    
+    _inicial = function() {
+        params = {
+          'comando':'carga_comboboxPais'  
+        };
+        method = 'POST';
+        url = 'modelado.php';
+        _ajaxCallback(params, method, url, _callback_cmbPais);
+    };
+    
+    _seg_combobox = function() {
+        seleccionado = $('#comboboxpais').val();
+        params = {
+            'comando': 'carga_comboboxCiudad',
+            'optPais': seleccionado
+        };
+        method = 'POST';
+        url = 'modelado.php';
+        _ajaxCallback(params, method, url, _callback_cmbCiudad);
+    };
+    
+    this.inicial = function() {
+        return _inicial();
+    };
+    
+    this.seg_combobox = function() {
+        return _seg_combobox();
+    };
+    
 };
 
-function seg_combobox(hola) {
-    seleccionado = $('#comboboxpais').val();
-    params = {
-        'comando': 'carga_comboboxCiudad',
-        'optPais': seleccionado
-    };
-    method = 'POST';
-    url = 'modelado.php';
-    ajaxCallback(params, method, url, callback_cmbCiudad);
-}
+caps = new Capsula();
 
-$(document).ready(inicial);
 
-$('#comboboxpais').on('change', seg_combobox);
+$(document).ready(caps.inicial);
+
+$('#comboboxpais').on('change', caps.seg_combobox);
 
 
--- modelado.php
+++ modelado.php
@@ -1,37 +1,55 @@
 <?php
 
 require 'conexion.php';
-$cmd = $_POST['comando'];
 
-switch ($cmd)
+class Capsula
 {
-    case 'carga_comboboxPais':
-        $pgsql = getConnect();
-        $query = "SELECT * FROM combobox.Pais";
-        $respuesta = $pgsql->prepare($query);
-        $respuesta->execute();
-        $lista = '<option value="0">Seleccionar Opción</option>';
-        while($f = $respuesta->fetchObject())
+    public function __construct($cmd, $optPais)
+    {
+        $this->cmd = $cmd;
+        $this->optPais = $optPais;
+    }
+    
+    private function _opciones()
+    {
+        switch ($this->cmd)
         {
-            $lista .='<option value="'.$f->idpais.'">'.$f->nombrepais.'</option>';
+            case 'carga_comboboxPais':
+                $pgsql = getConnect();
+                $query = "SELECT * FROM combobox.Pais";
+                $respuesta = $pgsql->prepare($query);
+                $respuesta->execute();
+                $lista = '<option value="0">Seleccionar Opción</option>';
+                while($f = $respuesta->fetchObject())
+                {
+                    $lista .='<option value="'.$f->idpais.'">'.$f->nombrepais.'</option>';
+                }
+               return $lista; 
+
+            case 'carga_comboboxCiudad':
+                $pgsql = getConnect();
+                $query = "SELECT * FROM combobox.Ciudad WHERE idpais='$this->optPais'";
+                $respuesta = $pgsql->prepare($query);
+                $respuesta->execute();
+                $lista = '<option value="0">Seleccionar Opción</option>';
+                while($f = $respuesta->fetchObject())
+                {
+                    $lista .='<option value="'.$f->idciudad.'">'.$f->nombreciudad.'</option>';
+                }
+               return $lista; 
+
+            default:
+                return;
         }
-       echo $lista; 
-       break;
-       
-    case 'carga_comboboxCiudad':
-        $optPais = $_POST['optPais'];
-        $pgsql = getConnect();
-        $query = "SELECT * FROM combobox.Ciudad WHERE idpais='$optPais'";
-        $respuesta = $pgsql->prepare($query);
-        $respuesta->execute();
-        $lista = '<option value="0">Seleccionar Opción</option>';
-        while($f = $respuesta->fetchObject())
-        {
-            $lista .='<option value="'.$f->idciudad.'">'.$f->nombreciudad.'</option>';
-        }
-       echo $lista; 
-       break;
-       
-    default:
-        break;
+    }
+    
+    public function opciones()
+    {
+        return $this->_opciones();
+    }
 }
+
+$cmd = $_POST['comando'];
+$optPais = $_POST['optPais'];
+$caps = new Capsula($cmd, $optPais);
+echo $caps->opciones();
